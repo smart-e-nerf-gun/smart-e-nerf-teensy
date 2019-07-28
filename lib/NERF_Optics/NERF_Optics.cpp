9b2c1cf8659751aab95b654978f36a7f4a132b08
Aditya Prabhakara <a.prabhakara96@gmail.com>
2019-07-28 18:47:08 +1000

    Resolve compile error for static members in optics.

--- lib/NERF_Optics/NERF_Optics.cpp
+++ lib/NERF_Optics/NERF_Optics.cpp
@@ -1,12 +1,9 @@
 #include "NERF_Optics.h"
 
-NERF_Optics::NERF_Optics (uint8_t sensor_1_pin, uint8_t sensor_2_pin) {
-
-    sensor_1 = sensor_1_pin;
-    sensor_2 = sensor_2_pin;
-    read_first_sensor = false;
-
-}
+bool NERF_Optics::read_first_sensor = false;
+volatile unsigned long NERF_Optics::time1 = 0;
+volatile unsigned long NERF_Optics::time2 = 0;
+volatile unsigned long NERF_Optics::duration = 0;
 
 void NERF_Optics::toggleLED_1() {
 
@@ -32,16 +29,16 @@ void NERF_Optics::toggleLED_2() {
     return;
 }
 
-void NERF_Optics::setupSensors() {
+void NERF_Optics::setupOptics() {
 
-    pinMode(sensor_1, INPUT_PULLUP);    // Configure the inputs with internal pullups
-    pinMode(sensor_2, INPUT_PULLUP);
+    pinMode(OPTIC_SENSOR_1_PIN, INPUT_PULLUP);    // Configure the inputs with internal pullups
+    pinMode(OPTIC_SENSOR_2_PIN, INPUT_PULLUP);
     
     read_first_sensor = false;          // Default, ready to read the first sensor first
 
     // Configure the ISR handlers
-    attachInterrupt(digitalPinToInterrupt(sensor_1), toggleLED_1, FALLING);
-    attachInterrupt(digitalPinToInterrupt(sensor_2), toggleLED_2, FALLING);
+    attachInterrupt(digitalPinToInterrupt(OPTIC_SENSOR_1_PIN), toggleLED_1, FALLING);
+    attachInterrupt(digitalPinToInterrupt(OPTIC_SENSOR_2_PIN), toggleLED_2, FALLING);
 
     return;
 
--- lib/NERF_Optics/NERF_Optics.h
+++ lib/NERF_Optics/NERF_Optics.h
@@ -1,23 +1,16 @@
 #pragma once
 
-#include <Arduino.h>
+#include <Includes.h>
 
 class NERF_Optics {
 
-    private:
-
-        static uint8_t sensor_1; // Sensor output voltage
-        static uint8_t sensor_2; // Sensor output voltage
-        static bool read_first_sensor;
-
     public:
 
+        static bool read_first_sensor;
         static volatile unsigned long time1, time2, duration;
 
-        static void setupSensors();
-        static void toggleLED_1 ();
-        static void toggleLED_2 ();
-
-        NERF_Optics (uint8_t sensor_1_pin, uint8_t sensor_2_pin);
+        static void toggleLED_1();
+        static void toggleLED_2();
+        static void setupOptics();
 
 };
