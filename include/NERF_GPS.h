8ecff76d32fc12e20a76fe63a7a08ecc114910c9
jheeettfire <jhettvivares@yahoo.com>
2019-08-14 15:49:51 +1000

    commit

--- include/NERF_GPS.h
+++ include/NERF_GPS.h
@@ -4,9 +4,32 @@
 #include <TinyGPS++.h>
 
 class NERF_GPS {
-
+    friend class TinyGPSPlus;
     private:
-
+        //Leave this empty for now as most information will be sent through anyway
     public:
 
+};
+
+//Location
+class NERF_Location{
+    friend class TinyGPSLocation;
+
+};
+
+//Time and Date Class
+class NERF_TimeDate{
+    friend class TinyGPSTime;
+    friend class TinyGPSDate;
+    private:
+        //Leave this empty for now
+    public:
+        double getDate(){ //For getting the date (optional)
+            uint32_t value();
+            uint16_t year();
+            uint8_t month();
+            uint8_t day();   
+
+        }
+        
 };
