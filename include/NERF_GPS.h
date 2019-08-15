b048fa39232b1552ff5c26f53b8033a7c0dc28cc
jheeettfire <jhettvivares@yahoo.com>
2019-08-15 18:00:19 +1000

    Update 15/08/2019 6PM

--- include/NERF_GPS.h
+++ include/NERF_GPS.h
@@ -4,17 +4,28 @@
 #include <TinyGPS++.h>
 
 class NERF_GPS {
-    friend class TinyGPSPlus;
+    friend class NERF_Location;
+    friend class NERF_TimeDate;
     private:
         //Leave this empty for now as most information will be sent through anyway
     public:
+        //This is pretty much the main class that would be called from the main.
+        //Everything here would be a mix of the class functions listed below
+        
 
 };
 
-//Location
-class NERF_Location{
-    friend class TinyGPSLocation;
+//Location class
+class NERF_Location : public TinyGPSLocation , public TinyGPSAltitude {  
     private:
+        struct locFormat{
+            //This will contain all the 
+            double lat; //Latitude
+            double lon; //Longitude
+            double alt; //Altitude
+
+            char dir; //Direction of point of reference (N, NE, E, SE, S, SW, W, NW (I think))
+        };
 
     public:
 
@@ -23,21 +34,38 @@ class NERF_Location{
 //Time and Date Class
 class NERF_TimeDate : public TinyGPSDate, public TinyGPSTime {
     private:
-        //Leave this empty for now
-    public:
         struct dateFormat{ //For passing back to Dashboard
-            uint16_t int1; //This will follow the DD/MM/YYYY format
-            uint16_t int2;
-            uint16_t int3;
+            //Date variables
+            uint16_t dd; //This will follow the DD/MM/YYYY format
+            uint16_t mm; //Month
+            uint16_t yy; //Year
+            uint16_t hh; //Hour
+
+            //Time variables
+            uint16_t mm; //Minutes
+            uint16_t ss; //Seconds? If possible
         };
+    public:
+        //Declare all the functions
+        dateFormat getDate(){ //For getting the date (optional)            
+            dateFormat format;
+            format.yy = year(); //TinyGPS year() function
+            format.mm = month(); //TinyGPS month() function
+            format.dd = day(); //TinyGPS day() function
 
-        uint16_t getDate(){ //For getting the date (optional)            
-            uint16_t curr_year = year();
-            uint16_t curr_month = month();
-            uint16_t curr_day = day();
-
-            dateFormat current_date = {curr_day, curr_month, curr_year};
-            return curr_day;
+            return format;
         }
+
+        dateFormat getTime(){ //Get time and have a format
+            dateFormat format;
+            format.hh = hour();
+            format.mm = minute(); 
+
+            return format;
+        }
+
+
+
+
         
 };
