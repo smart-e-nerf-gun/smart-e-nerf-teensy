8b67e74c77df16ad678f8e37c9980a0b57de30d7
Louie <drem.856@gmail.com>
2019-09-05 13:49:04 +1000

    Fixed UID

--- include/NERF_RFID.h
+++ include/NERF_RFID.h
@@ -7,6 +7,13 @@
 #define SS_PIN 10
 #define RST_PIN 9
 
+/*struct rfidInfo_struct
+{
+    uint8_t uidRead[10]; //UID to send to server
+    bool authorized; //0 is unauthorize, 1 is authorized
+    bool tagType; //0 for user tag, 1 for magazine tag
+
+};*/
 
 class NERF_RFID: public MFRC522 {
 
@@ -14,12 +21,17 @@ class NERF_RFID: public MFRC522 {
 
         uint8_t current_user;
         uint8_t current_magazine;
+        //rfidInfo_struct rfidInfo;
+        uint8_t uidRead[10]; //UID to send to server
+        bool authorized; //0 is unauthorize, 1 is authorized
+        bool tagType; //0 for user tag, 1 for magazine tag
 
     public:
-        void rfidSetup();
-        void checkAuthorisedUser();
-        void checkMagazineUser();
-
+        void rfidSetup(); //use to setup RFID
+        bool rfidAuthenticate();
+        void rfidRead();
+        //void checkMagazineUser();
+        
         uint8_t getCurrentUser();
         void setCurrentUser();
 
@@ -28,6 +40,6 @@ class NERF_RFID: public MFRC522 {
 
         NERF_RFID () {
             MFRC522(SS_PIN, RST_PIN);
-            current_user = 10;
+            //current_user = 10; //debugging purposes
         }
 };
