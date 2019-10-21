70d058736381a5fc178c52902cdb98f1768bb60e
Aditya Prabhakara <a.prabhakara96@gmail.com>
2019-10-22 10:53:41 +1100

    Initial implementaion

--- lib/NERF_IMU/NERF_IMU.cpp
+++ lib/NERF_IMU/NERF_IMU.cpp
@@ -1,1 +1,87 @@
 #include <NERF_IMU.h>
+
+void NERF_IMU::setupImu() {
+	settings.device.commInterface = IMU_MODE_I2C;
+	settings.device.mAddress = LSM9DS1_M;
+	settings.device.agAddress = LSM9DS1_AG;
+
+	if (!begin()) {
+		Serial.println("Failed to communicate with LSM9DS1.");
+		Serial.println("Double-check wiring.");
+		Serial.println("Default settings in this sketch will "
+					   "work for an out of the box LSM9DS1 "
+					   "Breakout, but may need to be modified "
+					   "if the board jumpers are.");
+		while (1)
+			;
+	}
+
+	Serial.println("Finished IMU set-up");
+}
+
+void NERF_IMU::updateImuData() {
+
+	// Update the sensor values whenever new data is available
+	
+	if (nerf_imu.gyroAvailable()) {
+		nerf_imu.readGyro();
+	}
+	if (nerf_imu.accelAvailable()) {
+		nerf_imu.readAccel();
+	}
+	if (nerf_imu.magAvailable()) {
+		nerf_imu.readMag();
+	}
+}
+
+// void NERF_IMU::getGyro() {
+//     Serial.println("Getting gyro");
+// 	if (gyroAvailable()) {
+//         Serial.println("Gyro available");
+// 		readGyro();
+// 	}
+// }
+
+// void NERF_IMU::getAxl() {
+//     Serial.println("Getting axl");
+// 	if (accelAvailable()) {
+//         Serial.println("axl available");
+// 		readGyro();
+// 	}
+// }
+
+// void NERF_IMU::getMag() {
+//     Serial.println("Getting mag");
+// 	if (magAvailable()) {
+//         Serial.println("mag available");
+// 		readMag();
+// 	}
+// }
+
+void NERF_IMU::printAttitude(float ax, float ay, float az, float mx, float my, float mz) {
+	float roll = atan2(ay, az);
+	float pitch = atan2(-ax, sqrt(ay * ay + az * az));
+
+	float heading;
+	if (my == 0)
+		heading = (mx < 0) ? PI : 0;
+	else
+		heading = atan2(mx, my);
+
+	heading -= DECLINATION * PI / 180;
+
+	if (heading > PI)
+		heading -= (2 * PI);
+	else if (heading < -PI)
+		heading += (2 * PI);
+
+	// Convert everything from radians to degrees:
+	heading *= 180.0 / PI;
+	pitch *= 180.0 / PI;
+	roll *= 180.0 / PI;
+
+	Serial.print("Pitch, Roll: ");
+	Serial.print(pitch, 2);
+	Serial.print(", ");
+	Serial.println(roll, 2);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,13 @@
 #include <NERF.h>
 #include <NERF_Display.h>
 #include <NERF_GPS.h>
+#include <NERF_IMU.h>
 #include <NERF_Optics.h>
 #include <NERF_RFID.h>
 #include <NERF_XBee.h>
 #include <ardprintf.h>
+
+#define PRINT_SPEED 1000 // 250 ms between prints
 
 char name[6];
 unsigned int shotcount;
@@ -17,6 +20,8 @@ uint8_t mag_id[4] = {0, 0, 0, 0};
 const char mf_label[8] = {'m', 'i', 's', 'f', 'i', 'r', 'e', 's'};
 const char clear_mf[12] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}; //12 spaces "_ _ _ _ m i s f i r e s"
 
+static unsigned long lastPrint = 0; // Keep track of print time
+
 void setup() {
 
 	Serial.begin(115200); //Setup everything here. Power up, initilization, etc.
@@ -26,20 +31,47 @@ void setup() {
 	// Serial.println("In setup");
 	// #endif
 
-	nerf_xbee.setUpXbee();
-	nerf_rfid.rfidSetup();
-	nerf_display.setupDisplay();
+	// nerf_xbee.setUpXbee();
+	// nerf_rfid.rfidSetup();
+	// nerf_display.setupDisplay();
 
 	// #ifdef DEBUG
 	// Serial.println("Finished debug");
 	// #endif
+	nerf_imu.setupImu();
 }
 
 void loop() {
 
+	nerf_imu.updateImuData();
+
+	if ((lastPrint + PRINT_SPEED) < millis()) {
+		// Print the heading and orientation for fun!
+		// Call print attitude. The LSM9DS1's mag x and y
+		// axes are opposite to the accelerometer, so my, mx are
+		// substituted for each other.
+		nerf_imu.printAttitude(nerf_imu.ax, nerf_imu.ay, nerf_imu.az,
+					  -nerf_imu.my, -nerf_imu.mx, nerf_imu.mz);
+		Serial.println();
+
+		lastPrint = millis(); // Update lastPrint time
+	}
+
+	// nerf_imu.getGyro();
+	// nerf_imu.getAxl();
+	// nerf_imu.getMag();
+	// nerf_imu.printAttitude(nerf_imu.ax, nerf_imu.ay, nerf_imu.az,
+	// 					   -nerf_imu.my, -nerf_imu.mx, -nerf_imu.mz);
+
+	// if ((lastPrint + PRINT_SPEED) < millis()) {
+	// 	nerf_imu.printAttitude(nerf_imu.ax, nerf_imu.ay, nerf_imu.az,
+	// 						   -nerf_imu.my, -nerf_imu.mx, -nerf_imu.mz);
+	// 	lastPrint = millis(); // Update lastPrint time
+	// }
+
 	// ardprintf("Current state: %d \n", current_state);
 
-	nerf_display.writeMF(random(0, 1000));
+	// nerf_display.writeMF(random(0, 1000));
 
 	// switch (current_state) {
 
@@ -84,7 +116,4 @@ void loop() {
 	// }
 
 	// current_state = next_state;
-
-	delay(1000);
-	
 }
