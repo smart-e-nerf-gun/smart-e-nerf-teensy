05cf3d123a663d51741781834c14179b7407dd1f
Louie <drem.856@gmail.com>
2019-10-22 15:43:20 +1100

    username display

--- lib/NERF_Display/NERF_Display.cpp
+++ lib/NERF_Display/NERF_Display.cpp
@@ -1,21 +1,23 @@
 #include "NERF_Display.h"
+#include <Fonts/FreeMono9pt7b.h>	  // For all other information
 #include <Fonts/FreeMonoBold24pt7b.h> //For Ammo count
-#include <Fonts/FreeMono9pt7b.h>  // For all other information
 
-void NERF_Display::setupDisplay() {
+void NERF_Display::setupDisplay()
+{
 	delay(100);
 	// SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-	if (!begin(SSD1306_SWITCHCAPVCC, 0x3D)) { // Address 0x3D for 128x64
+	if (!begin(SSD1306_SWITCHCAPVCC, 0x3D))
+	{ // Address 0x3D for 128x64
 		Serial.println(F("SSD1306 allocation failed"));
 		for (;;)
 			; // Don't proceed, loop forever
 	}
-	
+
 	setTextColor(WHITE); //Set text color. But its monochrome so there really is no other choice
-	setRotation(0); //Set orientation
-	setTextWrap(false); //To prevent long lines from wrapping around
-	dim(0); //Set brightness. 1 is too dim
-	clearDisplay(); //Clear display/buffer
+	setRotation(0);		 //Set orientation
+	setTextWrap(false);  //To prevent long lines from wrapping around
+	dim(0);				 //Set brightness. 1 is too dim
+	clearDisplay();		 //Clear display/buffer
 }
 
 static const unsigned char unauth[] = {
@@ -86,7 +88,8 @@ const unsigned char auth[] = {
 	0x00, 0x00, 0x00, 0x3F, 0xFE, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x07, 0xC0, 0x00, 0x00, 0x00,
 	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 
-void NERF_Display::display_unauth() {
+void NERF_Display::display_unauth()
+{
 	clearDisplay();
 
 	drawBitmap(
@@ -97,7 +100,8 @@ void NERF_Display::display_unauth() {
 	//delay(1000);
 }
 
-void NERF_Display::display_auth() {
+void NERF_Display::display_auth()
+{
 	clearDisplay();
 
 	drawBitmap(
@@ -108,89 +112,303 @@ void NERF_Display::display_auth() {
 	// delay(1000);
 }
 
-void NERF_Display::setupStaticText(){
+void NERF_Display::setupStaticText()
+{
 	cp437(true);
 	//misfire setup text
-	setCursor(misfirecursor[0],misfirecursor[1]);
-	write(0);write(0);write(0);write(0);write(109);write(105);write(115);write(115);//write "miss" in screen strting with blank space for number
+	setCursor(misfirecursor[0], misfirecursor[1]);
+	write(0);
+	write(0);
+	write(0);
+	write(0);
+	write(109);
+	write(105);
+	write(115);
+	write(115); //write "miss" in screen strting with blank space for number
 	display();
 
 	//shot count setup text
-	setCursor(shotcursor[0],shotcursor[1]);
-	write(0);write(0);write(0);write(0);write(115);write(104);write(111);write(116);write(115);//write "shots" in screen strting with blank space for number
+	setCursor(shotcursor[0], shotcursor[1]);
+	write(0);
+	write(0);
+	write(0);
+	write(0);
+	write(115);
+	write(104);
+	write(111);
+	write(116);
+	write(115); //write "shots" in screen strting with blank space for number
 	display();
 
 	//bullet speed setup text
-	setCursor(bulletcursor[0],bulletcursor[1]);
-	write(0);write(0);write(0);write(109);write(47);write(115);//write "m/s" in screen strting with blank space for number
+	setCursor(bulletcursor[0], bulletcursor[1]);
+	write(0);
+	write(0);
+	write(0);
+	write(109);
+	write(47);
+	write(115); //write "m/s" in screen strting with blank space for number
 	display();
-
 }
 
-void NERF_Display::invert_display(){
-  invertDisplay(true);
-  delay(250);
-  invertDisplay(false);
-  delay(250);
+void NERF_Display::invert_display()
+{
+	invertDisplay(true);
+	delay(250);
+	invertDisplay(false);
+	delay(250);
 }
 
-void NERF_Display::updateAC(int AC){
-	unsigned int AC_buffer=AC;
+void NERF_Display::updateUN(char UN[6])
+{
+	char UN_cp437[6];
+	setTextSize(1);
+	setTextColor(BLACK); // set text color to black
+	setCursor(usercursor[0], usercursor[1]);
+	write(219);	write(219); write(219); write(219); write(219); write(219);
+
+	setCursor(usercursor[0], usercursor[1]);	
+	for (size_t i = 0; i <6; i++)
+	{
+		switch (UN[i])
+		{
+			case 'A':
+				UN_cp437[i] = 65;
+				break;
+			case 'B':
+				UN_cp437[i] = 66;
+				break;
+			case 'C':
+				UN_cp437[i] = 67;
+				break;
+			case 'D':
+				UN_cp437[i] = 68;
+				break;
+			case 'E':
+				UN_cp437[i] = 69;
+				break;
+			case 'F':
+				UN_cp437[i] = 70;
+				break;
+			case 'G':
+				UN_cp437[i] = 71;
+				break;
+			case 'H':
+				UN_cp437[i] = 72;
+				break;
+			case 'I':
+				UN_cp437[i] = 73;
+				break;
+			case 'J':
+				UN_cp437[i] = 74;
+				break;
+			case 'K':
+				UN_cp437[i] = 75;
+				break;
+			case 'L':
+				UN_cp437[i] = 76;
+				break;
+			case 'M':
+				UN_cp437[i] = 77;
+				break;
+			case 'N':
+				UN_cp437[i] = 78;
+				break;
+			case 'O':
+				UN_cp437[i] = 79;
+				break;
+			case 'P':
+				UN_cp437[i] = 80;
+				break;
+			case 'Q':
+				UN_cp437[i] = 81;
+				break;
+			case 'R':
+				UN_cp437[i] = 82;
+				break;
+			case 'S':
+				UN_cp437[i] = 83;
+				break;
+			case 'T':
+				UN_cp437[i] = 84;
+				break;
+			case 'U':
+				UN_cp437[i] = 85;
+				break;
+			case 'V':
+				UN_cp437[i] = 86;
+				break;
+			case 'W':
+				UN_cp437[i] = 87;
+				break;
+			case 'X':
+				UN_cp437[i] = 88;
+				break;
+			case 'Y':
+				UN_cp437[i] = 89;
+				break;
+			case 'Z':
+				UN_cp437[i] = 90;
+				break;
+			case 'a':
+				UN_cp437[i] = 97;
+				break;
+			case 'b':
+				UN_cp437[i] = 98;
+				break;
+			case 'c':
+				UN_cp437[i] = 99;
+				break;
+			case 'd':
+				UN_cp437[i] = 100;
+				break;
+			case 'e':
+				UN_cp437[i] = 101;
+				break;
+			case 'f':
+				UN_cp437[i] = 102;
+				break;
+			case 'g':
+				UN_cp437[i] = 103;
+				break;
+			case 'h':
+				UN_cp437[i] = 104;
+				break;
+			case 'i':
+				UN_cp437[i] = 105;
+				break;
+			case 'j':
+				UN_cp437[i] = 106;
+				break;
+			case 'k':
+				UN_cp437[i] = 107;
+				break;
+			case 'l':
+				UN_cp437[i] = 108;
+				break;
+			case 'm':
+				UN_cp437[i] = 109;
+				break;
+			case 'n':
+				UN_cp437[i] = 110;
+				break;
+			case 'o':
+				UN_cp437[i] = 111;
+				break;
+			case 'p':
+				UN_cp437[i] = 112;
+				break;
+			case 'q':
+				UN_cp437[i] = 113;
+				break;
+			case 'r':
+				UN_cp437[i] = 114;
+				break;
+			case 's':
+				UN_cp437[i] = 115;
+				break;
+			case 't':
+				UN_cp437[i] = 116;
+				break;
+			case 'u':
+				UN_cp437[i] = 117;
+				break;
+			case 'v':
+				UN_cp437[i] = 118;
+				break;
+			case 'w':
+				UN_cp437[i] = 119;
+				break;
+			case 'x':
+				UN_cp437[i] = 120;
+				break;
+			case 'y':
+				UN_cp437[i] = 121;
+				break;
+			case 'z':
+				UN_cp437[i] = 122;
+				break;
+			default:
+				UN_cp437[i] = 0;
+				break;
+			
+		}
+		
+		
+		setTextColor(WHITE);
+		write(UN_cp437[i]);
+		
+		// for (size_t i = 0; i != 6; i++)
+		// {
+		// 	setTextColor(WHITE);
+		// 	//write(UN_cp437[i]);	
+		// 	//Serial.print(UN_cp437[i]);	
+		// 	//Serial.print(sizeof(UN));
+		// }
+		//Serial.println(UN_cp437[0]);Serial.println(UN_cp437[1]);Serial.println(UN_cp437[2]);Serial.println(UN_cp437[3]);Serial.println(UN_cp437[4]);Serial.println(UN_cp437[5]);Serial.println(UN_cp437[6]);
+	}
+	
+	
+}
+
+void NERF_Display::updateAC(int AC)
+{
+	unsigned int AC_buffer = AC;
 	setTextSize(4);
 
 	setTextColor(BLACK); // set text color to black
-  	setCursor(ammocursor[0],ammocursor[1]);
-  	write(219);write(219);
+	setCursor(ammocursor[0], ammocursor[1]);
+	write(219);	write(219);
 
 	setTextColor(WHITE);
-	setCursor(ammocursor[0],ammocursor[1]);
+	setCursor(ammocursor[0], ammocursor[1]);
 	print(AC_buffer);
 }
 
-void NERF_Display::updateMF(int MF){
-  unsigned int MF_buffer=MF;
-  setTextSize(1);
+void NERF_Display::updateMF(int MF)
+{
+	unsigned int MF_buffer = MF;
+	setTextSize(1);
 
-  setTextColor(BLACK); // set text color to black
-  setCursor(misfirecursor[0],misfirecursor[1]);
-  write(219);write(219);write(219);write(219); //make sure previous value is cleared by overwriting it with black pixels
+	setTextColor(BLACK); // set text color to black
+	setCursor(misfirecursor[0], misfirecursor[1]);
+	write(219);	write(219);	write(219);	write(219); //make sure previous value is cleared by overwriting it with black pixels
 
-  setTextColor(WHITE); // set text color to white
-  setCursor(misfirecursor[0],misfirecursor[1]);
-  print(MF_buffer);
-  
-  display();
+	setTextColor(WHITE); // set text color to white
+	setCursor(misfirecursor[0], misfirecursor[1]);
+	print(MF_buffer);
+
+	display();
 }
 
-void NERF_Display::updateTS(int TS){
-  unsigned int TS_buffer=TS;
-  setTextSize(1);
+void NERF_Display::updateTS(int TS)
+{
+	unsigned int TS_buffer = TS;
+	setTextSize(1);
 
-  setTextColor(BLACK); // set text color to black
-  setCursor(shotcursor[0],shotcursor[1]);
-  write(219);write(219);write(219);write(219); //make sure previous value is cleared by overwriting it with black pixels
+	setTextColor(BLACK); // set text color to black
+	setCursor(shotcursor[0], shotcursor[1]);
+	write(219);	write(219);	write(219);	write(219); //make sure previous value is cleared by overwriting it with black pixels
 
-  setTextColor(WHITE); // set text color to white
-  setCursor(shotcursor[0],shotcursor[1]);
-  print(TS_buffer);
-  
-  display();
+	setTextColor(WHITE); // set text color to white
+	setCursor(shotcursor[0], shotcursor[1]);
+	print(TS_buffer);
+
+	display();
 }
 
-void NERF_Display::updateBS(int BS){
-  unsigned int BS_buffer=BS;
-  setTextSize(1);
+void NERF_Display::updateBS(int BS)
+{
+	unsigned int BS_buffer = BS;
+	setTextSize(1);
 
-  setTextColor(BLACK); // set text color to black
-  setCursor(bulletcursor[0],bulletcursor[1]);
-  write(219);write(219);write(219); //make sure previous value is cleared by overwriting it with black pixels
+	setTextColor(BLACK); // set text color to black
+	setCursor(bulletcursor[0], bulletcursor[1]);
+	write(219);	write(219);	write(219); //make sure previous value is cleared by overwriting it with black pixels
 
-  setTextColor(WHITE); // set text color to white
-  setCursor(bulletcursor[0],bulletcursor[1]);
-  print(BS_buffer);
-  
-  display();
+	setTextColor(WHITE); // set text color to white
+	setCursor(bulletcursor[0], bulletcursor[1]);
+	print(BS_buffer);
+
+	display();
 }
-
-
-
